4e46ecb4ac17c186a457604b6a8def29c9695954
mid-kid <esteve.varela@gmail.com>
2022-06-11 13:37:58 +0200

    Decompile MATASK_EEPROM_Read

--- ma_api.c
+++ ma_api.c
@@ -8795,302 +8795,112 @@ void MA_EEPROMRead(u8 *unk_1)
     }
 }
 
-#if 0
-#else
-asm("
-.align 2
-.thumb_func
-MATASK_EEPROM_Read:
-    push	{r4, r5, lr}
-    ldr	r4, [pc, #48]
-    mov	r0, r4
-    add	r0, #69
-    ldrb	r0, [r0, #0]
-    cmp	r0, #238
-    bne	MATASK_EEPROM_Read+0x46
-    mov	r0, r4
-    add	r0, #80
-    ldrb	r0, [r0, #0]
-    cmp	r0, #17
-    beq	MATASK_EEPROM_Read+0x46
-    cmp	r0, #17
-    blt	MATASK_EEPROM_Read+0x38
-    cmp	r0, #25
-    bgt	MATASK_EEPROM_Read+0x38
-    cmp	r0, #24
-    blt	MATASK_EEPROM_Read+0x38
-    bl	MA_DefaultNegaResProc
-    mov	r0, r4
-    add	r0, #98
-    ldrb	r1, [r0, #0]
-    mov	r1, #250
-    b	MATASK_EEPROM_Read+0x44
-.align 2
-    .word gMA
+static void MATASK_EEPROM_Read(void)
+{
+    if (gMA.recv_cmd == (MACMD_ERROR | MAPROT_REPLY)) {
+        switch (gMA.unk_80) {
+        case 0x11:
+            break;
 
-    bl	MA_DefaultNegaResProc
-    ldr	r0, [pc, #36]
-    add	r0, #98
-    ldrb	r1, [r0, #0]
-    mov	r1, #251
-    strb	r1, [r0, #0]
-    ldr	r5, [pc, #28]
-    mov	r4, r5
-    add	r4, #98
-    ldrb	r0, [r4, #0]
-    cmp	r0, #3
-    beq	MATASK_EEPROM_Read+0xae
-    cmp	r0, #3
-    bgt	MATASK_EEPROM_Read+0x68
-    cmp	r0, #1
-    beq	MATASK_EEPROM_Read+0x84
-    cmp	r0, #1
-    bgt	MATASK_EEPROM_Read+0x94
-    cmp	r0, #0
-    beq	MATASK_EEPROM_Read+0x7e
-    b	MATASK_EEPROM_Read+0x252
-.align 2
-    .word gMA
+        case 0x18:
+        case 0x19:
+            MA_DefaultNegaResProc();
+            gMA.task_unk_98 = 0xfa;
+            break;
 
-    cmp	r0, #5
-    beq	MATASK_EEPROM_Read+0xf0
-    cmp	r0, #5
-    blt	MATASK_EEPROM_Read+0xda
-    cmp	r0, #250
-    bne	MATASK_EEPROM_Read+0x76
-    b	MATASK_EEPROM_Read+0x1a0
-    cmp	r0, #251
-    bne	MATASK_EEPROM_Read+0x7c
-    b	MATASK_EEPROM_Read+0x1ae
-    b	MATASK_EEPROM_Read+0x252
-    bl	MABIOS_Start
-    b	MATASK_EEPROM_Read+0x1a4
-    ldrb	r0, [r4, #0]
-    add	r0, #1
-    ldrb	r1, [r4, #0]
-    strb	r0, [r4, #0]
-    mov	r0, #1
-    bl	MABIOS_ChangeClock
-    b	MATASK_EEPROM_Read+0x252
-    mov	r1, #240
-    lsl	r1, r1, #1
-    add	r0, r5, r1
-    mov	r1, #0
-    strh	r1, [r0, #0]
-    mov	r1, r5
-    add	r1, #212
-    str	r1, [r0, #4]
-    mov	r1, #0
-    mov	r2, #128
-    bl	MABIOS_EEPROM_Read
-    b	MATASK_EEPROM_Read+0x1a4
-    ldr	r0, [r5, #112]
-    mov	r2, #242
-    lsl	r2, r2, #1
-    add	r1, r5, r2
-    ldr	r1, [r1, #0]
-    add	r1, #1
-    mov	r2, #128
-    bl	MAU_memcpy
-    mov	r1, #240
-    lsl	r1, r1, #1
-    add	r0, r5, r1
-    mov	r1, #0
-    strh	r1, [r0, #0]
-    mov	r1, r5
-    add	r1, #212
-    str	r1, [r0, #4]
-    mov	r1, #128
-    mov	r2, #64
-    bl	MABIOS_EEPROM_Read
-    b	MATASK_EEPROM_Read+0x1a4
-    ldr	r0, [r5, #112]
-    add	r0, #128
-    mov	r2, #242
-    lsl	r2, r2, #1
-    add	r1, r5, r2
-    ldr	r1, [r1, #0]
-    add	r1, #1
-    mov	r2, #64
-    bl	MAU_memcpy
-    b	MATASK_EEPROM_Read+0x1a0
-    mov	r0, #0
-    bl	MA_ChangeSIOMode
-    mov	r0, r5
-    add	r0, #92
-    ldrb	r1, [r0, #0]
-    mov	r4, #0
-    strb	r4, [r0, #0]
-    mov	r0, #0
-    bl	MA_ChangeSIOMode
-    ldrb	r0, [r5, #5]
-    lsl	r0, r0, #1
-    mov	r1, r5
-    add	r1, #8
-    add	r0, r0, r1
-    ldrh	r0, [r0, #0]
-    ldrh	r1, [r5, #12]
-    mov	r1, #0
-    strh	r0, [r5, #12]
-    str	r4, [r5, #60]
-    ldrb	r0, [r5, #4]
-    strb	r1, [r5, #4]
-    ldr	r0, [r5, #64]
-    mov	r1, #2
-    neg	r1, r1
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #96]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #92]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #88]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    mov	r1, #5
-    neg	r1, r1
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldrh	r1, [r5, #2]
-    ldr	r0, [pc, #72]
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    ldr	r0, [pc, #68]
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    mov	r4, #255
-    mov	r0, r4
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    bl	MAU_Socket_Clear
-    ldrh	r0, [r5, #2]
-    and	r4, r0
-    ldrh	r0, [r5, #2]
-    strh	r4, [r5, #2]
-    ldrh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    mov	r0, #0
-    mov	r1, #0
-    bl	MA_TaskSet
-    b	MATASK_EEPROM_Read+0x252
-.align 2
-    .word 0xfffffdff
-    .word 0xfffffbff
-    .word 0xffffdfff
-    .word 0x0000fff7
-    .word 0x0000ffef
+        default:
+            MA_DefaultNegaResProc();
+            gMA.task_unk_98 = 0xfb;
+            break;
+        }
+    }
 
-    bl	MABIOS_End
-    ldrb	r0, [r4, #0]
-    add	r0, #1
-    ldrb	r1, [r4, #0]
-    strb	r0, [r4, #0]
-    b	MATASK_EEPROM_Read+0x252
-    mov	r0, r5
-    add	r0, #92
-    ldrb	r1, [r0, #0]
-    mov	r4, #0
-    strb	r4, [r0, #0]
-    mov	r0, #0
-    bl	MA_ChangeSIOMode
-    ldrb	r0, [r5, #5]
-    lsl	r0, r0, #1
-    mov	r1, r5
-    add	r1, #8
-    add	r0, r0, r1
-    ldrh	r0, [r0, #0]
-    ldrh	r1, [r5, #12]
-    mov	r1, #0
-    strh	r0, [r5, #12]
-    str	r4, [r5, #60]
-    ldrb	r0, [r5, #4]
-    strb	r1, [r5, #4]
-    ldr	r0, [r5, #64]
-    mov	r1, #2
-    neg	r1, r1
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #116]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #112]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    ldr	r1, [pc, #108]
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldr	r0, [r5, #64]
-    mov	r1, #5
-    neg	r1, r1
-    and	r0, r1
-    str	r0, [r5, #64]
-    ldrh	r1, [r5, #2]
-    ldr	r0, [pc, #92]
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    ldr	r0, [pc, #88]
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    mov	r4, #255
-    mov	r0, r4
-    and	r0, r1
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    ldrh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    bl	MAU_Socket_Clear
-    ldrh	r0, [r5, #2]
-    and	r4, r0
-    ldrh	r0, [r5, #2]
-    strh	r4, [r5, #2]
-    ldrh	r0, [r5, #2]
-    ldrh	r1, [r5, #2]
-    strh	r0, [r5, #2]
-    mov	r0, r5
-    add	r0, #102
-    ldrb	r0, [r0, #0]
-    mov	r1, r5
-    add	r1, #104
-    ldrh	r1, [r1, #0]
-    bl	MA_SetApiError
-    mov	r0, #0
-    mov	r1, #0
-    bl	MA_TaskSet
-    pop	{r4, r5}
-    pop	{r0}
-    bx	r0
-.align 2
-    .word 0xfffffdff
-    .word 0xfffffbff
-    .word 0xffffdfff
-    .word 0x0000fff7
-    .word 0x0000ffef
-.size MATASK_EEPROM_Read, .-MATASK_EEPROM_Read
-");
-#endif
+    switch (gMA.task_unk_98) {
+    case 0:
+        MABIOS_Start();
+        gMA.task_unk_98++;
+        break;
+
+    case 1:
+        gMA.task_unk_98++;
+        MABIOS_ChangeClock(MA_SIO_WORD);
+        break;
+
+    case 2:
+        (&gMA.buffer_unk_480)->size = 0;
+        (&gMA.buffer_unk_480)->data = gMA.unk_212;
+        MABIOS_EEPROM_Read(&gMA.buffer_unk_480, 0, 0x80);
+        gMA.task_unk_98++;
+        break;
+
+    case 3:
+        MAU_memcpy(gMA.unk_112, &gMA.buffer_unk_480.data[1], 0x80);
+        (&gMA.buffer_unk_480)->size = 0;
+        (&gMA.buffer_unk_480)->data = gMA.unk_212;
+        MABIOS_EEPROM_Read(&gMA.buffer_unk_480, 0x80, 0x40);
+        gMA.task_unk_98++;
+        break;
+
+    case 4:
+        MAU_memcpy(&gMA.unk_112[0x80], &gMA.buffer_unk_480.data[1], 0x40);
+        MABIOS_End();
+        gMA.task_unk_98++;
+        break;
+
+    case 5:
+        MA_ChangeSIOMode(MA_SIO_BYTE);
+        gMA.unk_92 = 0;
+        MA_ChangeSIOMode(MA_SIO_BYTE);
+        gMA.timer_unk_12 = gMA.timer[gMA.sio_mode];
+        gMA.counter = 0;
+        gMA.intr_sio_mode = 0;
+        gMA.status &= ~STATUS_UNK_0;
+        gMA.status &= ~STATUS_UNK_9;
+        gMA.status &= ~STATUS_UNK_10;
+        gMA.status &= ~STATUS_UNK_13;
+        gMA.status &= ~STATUS_UNK_2;
+        gMA.condition &= ~MA_CONDITION_PTP_GET;
+        gMA.condition &= ~MA_CONDITION_CONNECT;
+
+        gMA.condition &= 0xff;
+        gMA.condition = gMA.condition;
+        MAU_Socket_Clear();
+        gMA.condition &= 0xff;
+        gMA.condition = gMA.condition;
+
+        MA_TaskSet(TASK_UNK_00, 0);
+        break;
+
+    case 0xfa:
+        MABIOS_End();
+        gMA.task_unk_98++;
+        break;
+
+    case 0xfb:
+        gMA.unk_92 = 0;
+        MA_ChangeSIOMode(MA_SIO_BYTE);
+        gMA.timer_unk_12 = gMA.timer[gMA.sio_mode];
+        gMA.counter = 0;
+        gMA.intr_sio_mode = 0;
+        gMA.status &= ~STATUS_UNK_0;
+        gMA.status &= ~STATUS_UNK_9;
+        gMA.status &= ~STATUS_UNK_10;
+        gMA.status &= ~STATUS_UNK_13;
+        gMA.status &= ~STATUS_UNK_2;
+        gMA.condition &= ~MA_CONDITION_PTP_GET;
+        gMA.condition &= ~MA_CONDITION_CONNECT;
+
+        gMA.condition &= 0xff;
+        gMA.condition = gMA.condition;
+        MAU_Socket_Clear();
+        gMA.condition &= 0xff;
+        gMA.condition = gMA.condition;
+
+        MA_SetApiError(gMA.unk_102, gMA.unk_104);
+        MA_TaskSet(TASK_UNK_00, 0);
+        break;
+    }
+}
 
 void MA_EEPROMWrite(u8 *unk_1)
 {
